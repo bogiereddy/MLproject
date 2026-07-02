6270f4aa9ad3e63770da34e22d789d20d2b61255
bogiereddy <bogiereddyobulreddy>
2026-07-02 23:49:47 +0530

    eda and pandas

--- src/utils.py
+++ src/utils.py
@@ -3,4 +3,8 @@ Prevents redundant code duplication .
 Houses reusable helper functions:
 (data_ingestion.py, data_transformation.py,
  model_trainer.py, and predict_pipeline.py)
+ 
+save_object() – Save trained models or preprocessors.
+load_object() – Load saved models or preprocessors.
+evaluate_models() – Train and compare multiple models.
  '''
